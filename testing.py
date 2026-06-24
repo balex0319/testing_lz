8b9307991152c77b32287317e5a37616eb1ee911
balex0319 <borissskk@tutamail.com>
2026-06-24 23:42:40 +0300

    testing.py

--- testing.py
+++ testing.py
@@ -1,32 +1,43 @@
 import pytest
-from testt import logarifm
+from tests import log
 
-if logarifm(1,5,2) == 5:
-    print('true')
-else :
-    print('false')
+if log(1,10,6) == 2:
+    print('True')
+else:
+    print('False')
 
-if logarifm(0.000001,1,2) <0:
+if log(0.1,2,2) < 0:
     print('true')
-else :
+else:
     print('false')    
 
-if logarifm(1,1,1) == True:
+if log(5, 5, 2) == 1:
     print('true')
-else :
+else:
     print('false')
 
-#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^РУЧНЫЕ ТЕСТЫ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+if log(1, 5, 2) == 0:
+    print('true')
+else:
+    print('false')
+
+if log(0,0,1) == True:
+    print('true')
+else:
+    print('false')
 
 
 def test_1():
-    
-    assert logarifm(1, 5, 2) == 5
+    assert log(1, 10, 6) == 2
 
 def test_2():
-    
-    assert logarifm(0.000001,1,2) <0
+    assert log(0.1, 2, 2) < 0
 
 def test_3():
-    
-    assert logarifm(1,1,1) == True
+    assert log(0, 0, 1) == True
+
+def test_4():
+    assert log(5, 5, 1) == 1
+
+def test_5():
+    assert log(1, 5, 1) == 0  
